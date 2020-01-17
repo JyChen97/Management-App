db996e43667011d362f000856511e3464f787b0d
JyChen97 <Jiayingchen1997@gmail.com>
2020-01-16 19:42:02 -0500

    Prepared for deployment

--- Server.js
+++ Server.js
@@ -2,6 +2,7 @@
 //This file is my server, run "node Server.js" to start up the server on the command line
 //Contains all the routes for my functionalities
 const express = require('express')
+const path = require('path')
 const bodyParser = require('body-parser')
 const timeStampRouter = require('./routers/Timestamp')
 const announcementRouter = require('./routers/AnnouncementBoard')
@@ -9,6 +10,7 @@ const updateInfoRouter = require('./routers/updateInfo')
 const timeScheduleRouter = require('./routers/ViewSchedule')
 const authRouter = require('./routers/Auth')
 const port = process.env.PORT || 5000
+
 
 const app = express()
 app.use(bodyParser.json())
@@ -20,6 +22,14 @@ app.use(updateInfoRouter)
 app.use(timeStampRouter)
 app.use(announcementRouter)
 app.use(authRouter)
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(port, () => {
   console.log('server listening on port:' + port )
